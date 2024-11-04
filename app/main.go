e28dcf48ba564c5fa4ac6b0159184b4f0c5478f0
mitsuki <mitsuki.highjump@gmail.com>
2024-11-05 02:17:48 +0900

    add: database config and handler

--- app/main.go
+++ app/main.go
@@ -1,17 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
 	"library-hub/app/adapter/api/action"
 	"library-hub/app/adapter/repository"
+	"library-hub/app/infrastructure/database"
 	"library-hub/app/usecase"
 	"log"
 	"net/http"
-	"os"
 	"time"
 )
 
@@ -21,21 +18,14 @@ func main() {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
 
-	dsn := fmt.Sprintf("%s:%s@tcp(localhost:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		os.Getenv("MYSQL_USER"),
-		os.Getenv("MYSQL_PW"),
-		os.Getenv("DB_HOST_PORT"),
-		os.Getenv("MYSQL_DB"))
-	fmt.Println(dsn)
-
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	mysql, err := database.NewMysqlHandler(database.NewConfigMySQL())
 	if err != nil {
-		log.Fatalf("Failed to connect database: %v", err)
+		log.Fatalf("Error connecting to mysql: %v", err)
 	}
 
 	r := mux.NewRouter()
 	uc := usecase.NewCreateCompanyInteractor(
-		repository.NewCompanyMySQL(db),
+		repository.NewCompanyMySQL(mysql.Db),
 	)
 	act := action.NewCreateCompanyAction(uc)
 	r.HandleFunc("/company", act.CreateCompany).Methods(http.MethodPost)
